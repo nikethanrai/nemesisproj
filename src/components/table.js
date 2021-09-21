9c02b8bbe27da43fbf9094dfd09ce1ef8a4c3d03
nikethanrai <nikethanrai58@gmail.com>
2021-09-21 20:16:11 +0530

    axa

---
+++ src/components/table.js
@@ -0,0 +1,52 @@
+import React,{useState} from 'react';
+import MaterialTable from 'material-table'
+
+const Table = () => {
+
+const [tableData,setTableData]=useState([
+    {name:'Leonard',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'},
+    {name:'Penny',username:'Pen45',email:'penny68@gmail.com',phone:9008950000,website:'penn.com'},
+    {name:'Lowdward',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'},
+    {name:'pord',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'}
+
+])
+
+ 
+const columns=[
+    {title:'Name',field:'name'},
+    {title:'Username',field:'username'},
+    {title:'Email',field:'email'},
+    {title:'Phone',field:'phone'},
+    {title:'Website',field:'website'}
+
+]
+    return (
+        <div>
+            <MaterialTable title='Nemesis DB'
+            data={tableData}
+            columns={columns}
+            editable={{
+                onRowAdd:(newRow)=>new Promise((resolve,reject)=>{
+                    setTableData([...tableData,newRow])
+                    setTimeout(()=>resolve(),500)
+
+                }),
+                onRowUpdate:(newRow,oldRow)=>new Promise((resolve,reject)=>{
+                    
+                    const updatedData=[...tableData]
+                    console.log(oldRow.tableData.id)
+                    updatedData[oldRow.tableData.id]=newRow
+                    setTableData(updatedData)
+                    setTimeout(()=>resolve(),500)
+                })
+            }}
+            options={{
+                exportButton:true,
+                addRowPosition:'first',
+                actionsColumnIndex:-1}}/>
+        </div>
+    );
+}
+
+
+export default Table;
