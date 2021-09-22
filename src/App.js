a708a19db1dfec210379c7202e67db1f576be527
nikethanrai <nikethanrai58@gmail.com>
2021-09-22 14:38:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Table from './components/table'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [users, setUsers] = useState();
+
+  const fetchUsers = async () => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await res.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <Table></Table>
+      {users && <Table users={users}></Table>}
     </div>
   );
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,55 +1,56 @@
-import React,{useState} from 'react';
+import React, { useState,useEffect } from 'react';
 import MaterialTable from 'material-table'
 
-const Table = () => {
+const Table = ({users}) => {
 
-const [tableData,setTableData]=useState([
-    {name:'Leonard',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'},
-    {name:'Penny',username:'Pen45',email:'penny68@gmail.com',phone:9008950000,website:'penn.com'},
-    {name:'Lowdward',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'},
-    {name:'pord',username:'leo45',email:'leonard34@gmail.com',phone:9000000000,website:'leon.com'}
+    const [tableData, setTableData] = useState()
 
-])
 
- 
-const columns=[
-    {title:'Name',field:'name'},
-    {title:'Username',field:'username'},
-    {title:'Email',field:'email'},
-    {title:'Phone',field:'phone'},
-    {title:'Website',field:'website'}
+    const columns = [
+        { title: 'Name', field: 'name' },
+        { title: 'Username', field: 'username' },
+        { title: 'Email', field: 'email' },
+        { title: 'Phone', field: 'phone' },
+        { title: 'Website', field: 'website' }
 
-]
+    ]
+
+    useEffect(() => {
+        setTableData(users);
+        console.log(users);
+    }, [users]);
+
     return (
         <div>
             <MaterialTable title='Nemesis DB'
-            data={tableData}
-            columns={columns}
-            editable={{
-                onRowAdd:(newRow)=>new Promise((resolve,reject)=>{
-                    setTableData([...tableData,newRow])
-                    setTimeout(()=>resolve(),500)
+                data={tableData}
+                columns={columns}
+                editable={{
+                    onRowAdd: (newRow) => new Promise((resolve, reject) => {
+                        setTableData([...tableData, newRow])
+                        setTimeout(() => resolve(), 500)
 
-                }),
-                onRowUpdate:(newRow,oldRow)=>new Promise((resolve,reject)=>{
-                    
-                    const updatedData=[...tableData]
-                    updatedData[oldRow.tableData.id]=newRow
-                    setTableData(updatedData)
-                    setTimeout(()=>resolve(),500)
-                }),
-                onRowDelete:(selectedRow)=>new Promise((resolve,reject)=>{
-                    const updatedData=[...tableData]
-                    updatedData.splice(selectedRow.tableData.id,1)
-                    setTableData(updatedData)
-                    setTimeout(()=>resolve(),1000)
-                })
-            }}
+                    }),
+                    onRowUpdate: (newRow, oldRow) => new Promise((resolve, reject) => {
 
-            options={{
-                exportButton:true,
-                addRowPosition:'first',
-                actionsColumnIndex:-1}}/>
+                        const updatedData = [...tableData]
+                        updatedData[oldRow.tableData.id] = newRow
+                        setTableData(updatedData)
+                        setTimeout(() => resolve(), 500)
+                    }),
+                    onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
+                        const updatedData = [...tableData]
+                        updatedData.splice(selectedRow.tableData.id, 1)
+                        setTableData(updatedData)
+                        setTimeout(() => resolve(), 1000)
+                    })
+                }}
+
+                options={{
+                    exportButton: true,
+                    addRowPosition: 'first',
+                    actionsColumnIndex: -1
+                }} />
         </div>
     );
 }
